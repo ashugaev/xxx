ff281ceecae12ada5659f5884df10f4a16c71f42
Шугаев Алексей Андреевич <a.shugaev@carprice.ru>
2018-11-20 23:39:50 +0300

    fix(heder): добавил деструктуризацию пропсов и отключил eslint на строке

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,21 +4,25 @@ import { changeTestState } from '../../actions/header';
 import './index.scss';
 
 function Header(props) {
+  const { someData } = props;
   return (
     <div>
       <b> Хедер</b>
       <br />
+      { /* eslint-disable-next-line */ }
       <button onClick={props.changeTestState}>Поменять текст</button>
       <br />
-      <span>{props.testState.someData}</span>
+      <span>{someData}</span>
     </div>
   );
 }
 
 const mapStateToProps = state => ({
-  testState: state.testState,
+  someData: state.testState.someData,
 });
 
-
-export default connect(mapStateToProps, { changeTestState })(Header);
+export default connect(
+  mapStateToProps,
+  { changeTestState },
+)(Header);
 // export default Header
